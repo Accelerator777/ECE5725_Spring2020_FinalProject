b58728002ef737c955f6edaab25871a73d84ad02
Accelerator777 <49878895+Accelerator777@users.noreply.github.com>
2020-05-05 04:07:19 -0400

    Update free_mode.py

--- free_mode.py
+++ free_mode.py
@@ -26,6 +26,9 @@ trellis = NeoTrellis(i2c_bus)
 print("NeoTrellis created")
 
 path = ""
+instrument = ""
+
+audio_file = None
 
 COLORS = ["RED", "YELLOW", "GREEN", "CYAN", "BLUE", "PURPLE", "WHITE"]
 COLOR_TUPLES = [RED, YELLOW, GREEN, CYAN, BLUE, PURPLE, WHITE]
@@ -38,70 +41,95 @@ Shuffled = False
 wavnames = ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""]
 shuffled_names = list(wavnames)  # Duplicate list, wavnames is our reference
 
-audio_file = None
-
-pygame.mixer.pre_init(44100, -16, 2, 2048)
-pygame.mixer.init()
-
 def play_file(audio_filename):
-	pygame.mixer.music.load(path+audio_filename)
-	pygame.mixer.music.play()
+    global audio_file
+    if audio_file:
+        audio_file.close()
+    audio_name = path+audio_filename
+    print(audio_file)
+    audio_file = open(path+audio_filename, "rb")
+    print("Playing "+audio_filename+".")
+    os.system('omxplayer '+audio_name+' &')
 
 # this will be called when button events are received
 def blink(event):
-	# turn the LED on when a rising edge is detected
-	if event.edge == NeoTrellis.EDGE_RISING:  # Trellis button pushed
-		trellis.pixels[event.number] = WHITE
-		if shuffled_names[event.number] != "":
-			play_file(shuffled_names[event.number])
-			print("START MUSIC")
-	# turn the LED off when a rising edge is detected
-	if event.edge == NeoTrellis.EDGE_FALLING:
-		print("STOP MUSIC")
-		pygame.mixer.music.stop()	
-		trellis.pixels[event.number] = shuffled_colors[event.number]
+    # turn the LED on when a rising edge is detected
+    if event.edge == NeoTrellis.EDGE_RISING:  # Trellis button pushed
+        print("Button "+str(event.number)+" pushed")
+        if event.number > 15:
+            print("Event number out of range: ", event.number)
+        trellis.pixels[event.number] = WHITE
+        if shuffled_names[event.number] != "":
+            play_file(shuffled_names[event.number])
+    # turn the LED off when a rising edge is detected
+    elif event.edge == NeoTrellis.EDGE_FALLING:
+        trellis.pixels[event.number] = shuffled_colors[event.number]
 
-path = "/home/pi/Final/piano/"
-wavefiles = [file for file in os.listdir(path) if (file.endswith(".ogg") and not file.startswith("._"))]
-if len(wavefiles) < 1:
-	print("No wav files found in sounds directory")
-else:
-	print("Audio files found: ", wavefiles)
+def run(instrument):
+    global path,wavnames,shuffled_names,buttons,button_colors,shuffled_colors,Shuffled
+    path = "/home/pi/Final/"+instrument+"/"
+    buttons = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    button_colors = [OFF, OFF, OFF, OFF, OFF, OFF, OFF, OFF, OFF, OFF, OFF, OFF, OFF, OFF, OFF, OFF]
+    shuffled_colors = list(button_colors)
+    Shuffled = False
 
-shuffled = False
-for soundfile in wavefiles:
-	print("Processing "+soundfile)
-	pos = int(soundfile[0:2])
-	if pos >= 0 and pos < 16:      # Valid filenames start with 00 to 15
-		wavnames[pos] = soundfile  # Store soundfile in proper index
-		shuffled_names[pos] = soundfile
-		skip = soundfile[3:].find('-') + 3
-		user_color = soundfile[3:skip].upper()  # Detect file color
-		print("For file "+soundfile+", color is "+user_color+".")
-		file_color = COLOR_TUPLES[COLORS.index(user_color)]
-		button_colors[pos] = file_color
-		shuffled_colors[pos] = file_color
-	else:
-		print("Filenames must start with a number from 00 to 15 - "+soundfile)
+    wavnames = ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""]
+    shuffled_names = list(wavnames)  # Duplicate list, wavnames is our reference
 
-for i in range(16):
-	# activate rising edge events on all keys
-	trellis.activate_key(i, NeoTrellis.EDGE_RISING)
-	# activate falling edge events on all keys
-	trellis.activate_key(i, NeoTrellis.EDGE_FALLING)
-	# set all keys to trigger the blink callback
-	trellis.callbacks[i] = blink
+    wavefiles = [file for file in os.listdir(path)
+                if (file.endswith(".wav") and not file.startswith("._"))]
+    if len(wavefiles) < 1:
+        print("No wav files found in sounds directory")
+    else:
+        print("Audio files found: ", wavefiles)
 
-	# cycle the LEDs on startup
-	trellis.pixels[i] = WHITE
-	time.sleep(0.05)
+    shuffled = False
+    for soundfile in wavefiles:
+        print("Processing "+soundfile)
+        pos = int(soundfile[0:2])
+        if pos >= 0 and pos < 16:      # Valid filenames start with 00 to 15
+            wavnames[pos] = soundfile  # Store soundfile in proper index
+            shuffled_names[pos] = soundfile
+            skip = soundfile[3:].find('-') + 3
+            user_color = soundfile[3:skip].upper()  # Detect file color
+            print("For file "+soundfile+", color is "+user_color+".")
+            file_color = COLOR_TUPLES[COLORS.index(user_color)]
+            button_colors[pos] = file_color
+            shuffled_colors[pos] = file_color
+        else:
+            print("Filenames must start with a number from 00 to 15 - "+soundfile)
+
+    for i in range(16):
+        # activate rising edge events on all keys
+        trellis.activate_key(i, NeoTrellis.EDGE_RISING)
+        # activate falling edge events on all keys
+        trellis.activate_key(i, NeoTrellis.EDGE_FALLING)
+        # set all keys to trigger the blink callback
+        trellis.callbacks[i] = blink
  
-for i in range(16):
-	trellis.pixels[i] = shuffled_colors[i]
-	time.sleep(0.05)
+        # cycle the LEDs on startup
+        trellis.pixels[i] = WHITE
+        time.sleep(0.05)
+ 
+    for i in range(16):
+        trellis.pixels[i] = shuffled_colors[i]
+        time.sleep(0.05)
 
-while True:
-	# call the sync function call any triggered callbacks
-	trellis.sync()
-	# the trellis can only be read every 17 milliseconds or so
-	time.sleep(0.001)
+    while True:
+        # call the sync function call any triggered callbacks
+        trellis.sync()
+        # the trellis can only be read every 17 milliseconds or so
+        time.sleep(.02)
+        for event in pygame.event.get():
+            if(event.type is MOUSEBUTTONUP):
+                pos=pygame.mouse.get_pos()
+                x,y=pos
+                if y>200:
+                    if x<80:
+                        return 1
+                    elif x>200:
+                        for i in range(16):
+                            trellis.pixels[i] = OFF
+                        return 0
+
+
